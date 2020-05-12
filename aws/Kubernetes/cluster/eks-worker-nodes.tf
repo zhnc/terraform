4c52dea5e7a236c994001bf8a02f9cf41d46e497
EC2 Default User <ec2-user@ip-172-31-7-60.cn-northwest-1.compute.internal>
2020-05-12 10:02:03 +0000

    update bj region policy_arn aws-cn

--- aws/Kubernetes/cluster/eks-worker-nodes.tf
+++ aws/Kubernetes/cluster/eks-worker-nodes.tf
@@ -54,22 +54,22 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "worker-node-AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  policy_arn = "arn:aws-cn:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = "${aws_iam_role.worker-node-role.name}"
 }
 
 resource "aws_iam_role_policy_attachment" "worker-node-AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  policy_arn = "arn:aws-cn:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = "${aws_iam_role.worker-node-role.name}"
 }
 
 resource "aws_iam_role_policy_attachment" "worker-node-AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  policy_arn = "arn:aws-cn:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = "${aws_iam_role.worker-node-role.name}"
 }
 
 resource "aws_iam_role_policy_attachment" "worker-node-AmazonEC2FullAccess" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+  policy_arn = "arn:aws-cn:iam::aws:policy/AmazonEC2FullAccess"
   role       = "${aws_iam_role.worker-node-role.name}"
 }
 
