71d79a1393b208300babe83d705a768007495890
EC2 Default User <ec2-user@ip-172-31-7-60.cn-northwest-1.compute.internal>
2020-05-12 09:26:44 +0000

    bj region only 2 AZ availible

--- aws/Kubernetes/main.tf
+++ aws/Kubernetes/main.tf
@@ -9,10 +9,10 @@ module "vpc" {
   cidr                = "10.0.0.0/16"
   vpc_name            = "${var.vpc_name}"
   cluster_name        = "${var.cluster-name}"
-  master_subnet_cidr  = ["10.0.48.0/20", "10.0.64.0/20", "10.0.80.0/20"]
-  worker_subnet_cidr  = ["10.0.144.0/20", "10.0.160.0/20", "10.0.176.0/20"]
-  public_subnet_cidr  = ["10.0.204.0/22", "10.0.208.0/22", "10.0.212.0/22"]
-  private_subnet_cidr = ["10.0.228.0/22", "10.0.232.0/22", "10.0.236.0/22"]
+  master_subnet_cidr  = [ "10.0.64.0/20", "10.0.80.0/20"]
+  worker_subnet_cidr  = [ "10.0.160.0/20", "10.0.176.0/20"]
+  public_subnet_cidr  = [ "10.0.208.0/22", "10.0.212.0/22"]
+  private_subnet_cidr = [ "10.0.232.0/22", "10.0.236.0/22"]
 }
 
 module "kubernetes-server" {
