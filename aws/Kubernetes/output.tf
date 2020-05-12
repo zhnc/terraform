63b765dec73cfa327866c105197ff554af3778fa
EC2 Default User <ec2-user@ip-172-31-7-60.cn-northwest-1.compute.internal>
2020-05-12 09:42:36 +0000

    fix subnet ids, debug it in output.tf

--- aws/Kubernetes/output.tf
+++ aws/Kubernetes/output.tf
@@ -4,3 +4,10 @@ output "Kubenetes-server-ip" {
   value = "${module.kubernetes-server.elastic_ip}"
 }
 
+output "subnet_ids"{
+  value = ["${module.vpc.master_subnet}", "${module.vpc.worker_node_subnet}"]
+}
+
+output "fix_subnet_ids"{
+  value = concat("${module.vpc.master_subnet}"[0], "${module.vpc.worker_node_subnet}"[0])
+}
